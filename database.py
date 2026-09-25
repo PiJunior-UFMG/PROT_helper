eb245c75ce0649c0b5b25940a4b12020e0d770ea
gdon <rpg.email.next@gmail.com>
2026-09-25 00:50:07 -0300

    fix: conectando db ao postgresql

--- database.py
+++ database.py
@@ -1,11 +1,21 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+load_dotenv()
 
-engine = create_async_engine(DATABASE_URL)
-#engine = create_async_engine(DATABASE_URL, echo=True) usar e debug
+DB_HOST = os.getenv("DB_HOST", "25.46.73.186")
+DB_PORT = os.getenv("DB_PORT", "5434")
+DB_USER = os.getenv("DB_USER", "pi-dev")
+DB_PASS = os.getenv("DB_PASS", "601285")
+DB_NAME = os.getenv("DB_NAME", "pi-db")
+
+# String de conexão assíncrona para o PostgreSQL usando asyncpg
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_async_engine(DATABASE_URL, echo=False)
 
 AsyncSessionLocal = sessionmaker(
     bind=engine,
@@ -23,4 +33,4 @@ async def get_db():
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    print("tabelas criadas com sucesso")
+    print("Tabelas criadas com sucesso no PostgreSQL!")
